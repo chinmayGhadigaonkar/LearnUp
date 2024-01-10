4a98afc798a97be00fd431f0489e98e424aea89e
chinmayGhadigaonkar <ghadigaonkarchinmay@gmail.com>
2024-01-10 13:04:39 +0530

    Make server Files Seprate

--- server/routes/ClerkAuthRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import ClerkAuth from "../models/clerkauth.js";
-import { adduser, finduser, getuser } from "../controllers/Clerk.js";
-
-const clerkauth = Router();
-
-clerkauth.get("/getuser", getuser).get("/getemail", finduser);
-clerkauth.post("/adduser", adduser);
-export default clerkauth;
